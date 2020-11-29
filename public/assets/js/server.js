a005ec155ffeb1dacb3b15917b9ba4cb57efe04a
Sishir <sishir.bhattarai@outlook.com>
2020-11-29 00:08:54 -0700

    added function to handle request and response

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,9 @@
 // We require/import the HTTP module
 var http = require("http");
 
+//Need fs
+var fs = require("fs");
+
 //Defining port
 var PORT = 8080;
 
@@ -14,5 +17,22 @@ server.listen(PORT, function() {
 
 //function to handles incoming requests
 function handleRequest(request, response){
-    response.end("You got it!")
+    response.end("Welcome to my world!")
+// Capture the url the request is made to 
+ var path = request.url;
+
+//// When we visit different urls, read and respond with different files
+switch(path) {
+
+case "/notes":
+    return fs.readFile(__dirname + "./notes.html", function(err, data) {
+        if(err) throw err;
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(data);
+    });
+
+
+
+ }
 }
+
