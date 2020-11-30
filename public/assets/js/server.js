29b24bd530111e5aef2fbc8f051337b0a0ec467c
Sishir <sishir.bhattarai@outlook.com>
2020-11-29 23:19:12 -0700

    added code

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,38 +1,32 @@
-// We require/import the HTTP module
-var http = require("http");
 
-//Need fs
-var fs = require("fs");
+app.use(express.static('public'))
 
-//Defining port
-var PORT = 8080;
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
 
-//Creating server 
-var server = http.createServer(handleRequest);
+// Sets up the Express App
+// =============================================================
+const  app = express();
+const  PORT = process.env.PORT || 3000;
 
-//Start our server
-server.listen(PORT, function() {
-      console.log("Server listening on: http://localhost:" + PORT);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", function(req, res) {
+	res.sendFile(path.join(__dirname, "../../notes.html"));
+});
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
-//function to handles incoming requests
-function handleRequest(request, response){
-    response.end("Welcome to my world!")
-// Capture the url the request is made to 
- var path = request.url;
-
-//// When we visit different urls, read and respond with different files
-switch(path) {
-
-case "/notes":
-    return fs.readFile(__dirname + "./notes.html", function(err, data) {
-        if(err) throw err;
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(data);
-    });
 
 
 
- }
-}
-
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+  });
