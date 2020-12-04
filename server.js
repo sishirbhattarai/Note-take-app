2287d9a740ea44419331af591a5bc7e74b1f3a29
Sishir <sishir.bhattarai@outlook.com>
2020-12-04 16:18:18 -0700

    added files

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ var fs = require("fs");
 const  app = express();
 const  PORT = process.env.PORT || 3000;
 
-var ID = function () {
-	// Math.random should be unique because of its seeding algorithm.
-	// Convert it to base 36 (numbers + letters), and grab the first 9 characters
-	// after the decimal.
-	return '_' + Math.random().toString(36).substr(2, 9);
-  };
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
