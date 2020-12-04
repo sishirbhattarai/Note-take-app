90647da5ec82027820b39eb4010c45ee657efc74
Sishir <sishir.bhattarai@outlook.com>
2020-12-04 10:34:58 -0700

    fixed codes for get and post

--- server.js
+++ server.js
@@ -2,11 +2,19 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
 const  app = express();
 const  PORT = process.env.PORT || 3000;
+
+var ID = function () {
+	// Math.random should be unique because of its seeding algorithm.
+	// Convert it to base 36 (numbers + letters), and grab the first 9 characters
+	// after the decimal.
+	return '_' + Math.random().toString(36).substr(2, 9);
+  };
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -24,9 +32,26 @@ app.get("/api/notes", function(req, res) {
 	res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-app.get("/", function(req, res) {
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[req.params.id]);
+});
+
+app.get("*", function(req, res) {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.post("/api/notes", function(req, res) {
+	let savedNotes = JSON.parse(fs.readFileSync("./db/db.JSON", "utf8"));
+	var newNote = req.body;
+	savedNotes.push(newNote);
+
+	fs.writeFileSync("./db/db.JSON", JSON.stringify(savedNotes));
+	console.log("Your notes is now saved", newNote);
+	res.json(savedNotes);
+});
+
+
 
 // Starts the server to begin listening
 // =============================================================
